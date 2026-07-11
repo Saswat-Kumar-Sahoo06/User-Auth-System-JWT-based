f903410c37f371d1f679e7ae037d6b8dfaa2b662
Saswat-Kumar-Sahoo06 <saswatkumarsahoo656@gmail.com>
2026-07-11 23:57:06 +0530

    Update user.controller.js

--- backend/src/controllers/user.controller.js
+++ backend/src/controllers/user.controller.js
@@ -95,8 +95,8 @@ export const loginUser = async (req, res) => {
     
     const options = {
         httpOnly: true,
-        secure: false,
-        sameSite: "lax",
+        secure: true,
+        sameSite: "none",
     }
 
     return res.status(200)
